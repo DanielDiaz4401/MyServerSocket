15ebf01345ccf29ab0d5297ec72b3c5fc61477a8
DanielDiaz44 <danieldiaz4401@gmail.com>
2023-06-11 22:13:54 +0200

    Finished P2

---
+++ ClientChat.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import util.*;
+public class ClientChat {
+    public static String username;
+    public static void main(String[] args){        
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("Escriu el teu nom: ");
+
+        try{
+            username = input.readLine();
+            System.out.println("Benvingut " + username);
+        }catch(Exception e){
+            System.out.println("Error llegint el teu usuari");
+        }
+
+        MySocket sc = new MySocket(username);        
+        
+        Thread socketThread = new Thread(() -> {
+            try{
+                String line;
+                while ((line = input.readLine()) != null){
+                    sc.write(line);                                        
+                }
+            }catch (Exception e){
+                System.out.println("Error llegint el teu missatge");
+            }
+        });
+        socketThread.start();
+
+        Thread keyboardThread =new Thread(() -> {
+            String line;
+            while((line = sc.read())!= null){ 
+                System.out.println(line);
+            }
+        });  
+        keyboardThread.start();
+    }
+}
---
+++ ServerChat.java
@@ -0,0 +1,35 @@
+import util.ConnectionTable;
+import util.MyServerSocket;
+import util.MySocket;
+
+public class ServerChat {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    
+    public static void main(String[] args){
+        MyServerSocket ss = new MyServerSocket();
+        ConnectionTable table = new ConnectionTable();
+        System.out.println("Esperant clients...");
+
+        while(true){
+            MySocket sc = ss.accept();
+
+            Thread handlerThread = new Thread(() -> {
+                String nom_usuari=sc.read();                
+                String usuari = table.addParticipant(nom_usuari, sc);
+                System.out.println(ANSI_GREEN + usuari + " ha entrat" + ANSI_RESET);
+                String content;
+                while((content=sc.read())!=null){
+                    table.broadcast(content, usuari);
+                    System.out.println(nom_usuari +": "+ content);             
+                }
+                System.out.println(ANSI_RED + usuari + " ha marxat" + ANSI_RESET);
+                table.removeParticipant(usuari);
+                sc.close();
+            });
+            handlerThread.start();
+
+        }
+    }
+}
